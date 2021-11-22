7b2567edd6b02882501ff059c9b4a96e8a3ae334
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-22 16:26:33 +0200

    second-commit

---
+++ controllers/productsController.js
@@ -0,0 +1,14 @@
+const Product = require('../models/productsModel')
+
+
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll()
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(products))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { getProducts }
--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http')
+const { getProducts } = require('./controllers/productsController')
 
 
 const server = http.createServer((req, res) => {
+    if (req.url === '/api/products' && req.method === 'GET') {
+        getProducts(req, res)
+    } else {
+        res.writeHead(404, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify({ message: 'Page not found' }))
+    }
+
 })
 
 const PORT = process.env.PORT || 5000

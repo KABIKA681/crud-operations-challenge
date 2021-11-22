ce987033906067c763576d6529c90d0a11957a68
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-22 16:53:58 +0200

    getSingle-productById

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,6 +1,8 @@
 const Product = require('../models/productsModel')
 
 
+
+//GET ALL PRODUCTS
 async function getProducts(req, res) {
     try {
         const products = await Product.findAll()
@@ -11,4 +13,26 @@ async function getProducts(req, res) {
     }
 }
 
-module.exports = { getProducts }
+//GET SINGLE PRODUCT 
+async function getProduct(req, res, id) {
+    try {
+        const product = await Product.findById()
+
+        if (!product) {
+            res.writeHead(404, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify({ message: 'Product Not Found' }))
+        } else {
+            res.writeHead(200, { 'Content-Type': 'application/json' })
+            res.end(JSON.stringify(product))
+
+        }
+
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { getProducts, getProduct }

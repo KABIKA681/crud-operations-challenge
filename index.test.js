738178e30238427cf4634058944a8ea835623cbf
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-23 12:13:50 +0200

    serverPortAlreadyInUse

--- index.test.js
+++ index.test.js
@@ -4,5 +4,14 @@ const server = require('./server')
 describe('Products', () => {
     it('GET /api/products --> get all the products', () => { });
     
-    it('GET /api/products/id --> specii')
+    it('GET /api/products/id --> fetch specific product by Id', () => { });
+        
+    it('POST /api/products/ --> post a product', () => { });
+
+    it('PUT /api/products/id --> Update a specific product by Id', () => { });
+
+
+    it('DELETE /api/products/id --> delete a specific product by Id', () => { });
+
+
 })

d62bf3142c1725693fc03b26ee69486ecc93a57d
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-23 12:18:48 +0200

    adding jest to package.json

--- index.test.js
+++ index.test.js
@@ -1,17 +1,17 @@
 const request = require('supertest')
-const server = require('./server')
+const app = require('')
 
 describe('Products', () => {
-    it('GET /api/products --> get all the products', () => { });
+    // it('GET /api/products --> get all the products', () => { });
     
-    it('GET /api/products/id --> fetch specific product by Id', () => { });
+    // it('GET /api/products/id --> fetch specific product by Id', () => { });
         
-    it('POST /api/products/ --> post a product', () => { });
+    // it('POST /api/products/ --> post a product', () => { });
 
-    it('PUT /api/products/id --> Update a specific product by Id', () => { });
+    // it('PUT /api/products/id --> Update a specific product by Id', () => { });
 
 
-    it('DELETE /api/products/id --> delete a specific product by Id', () => { });
+    // it('DELETE /api/products/id --> delete a specific product by Id', () => { });
 
 
 })

ca22972b15d3fc3b427cbd72401e7265cd681fb2
KABIKA681 <pascalkabikampan@gmail.com>
2021-11-23 12:05:50 +0200

    trying to do test

---
+++ index.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest')
+const server = require('./server')
+
+describe('Products', () => {
+    it('GET /api/products --> get all the products', () => { });
+    
+    it('GET /api/products/id --> specii')
+})
